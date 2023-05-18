0223813c10b7bc70d39f196961f7b67d0914a576
Bjorn De Bakker <debakkerbjorn@gmail.com>
2023-05-18 18:29:52 +0200

    Add license

--- modules/create.go
+++ modules/create.go
@@ -7,6 +7,22 @@ import (
 	"sort"
 )
 
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 func StartWizard() error {
 	fmt.Println("Create a module instance")
 	fmt.Println("#########################")
@@ -39,15 +55,4 @@ func getModuleNames(modules map[string]*terraform.Module) []string {
 	sort.Strings(moduleNames)
 
 	return moduleNames
-}
-
-func getMissingValues(module *terraform.Module) map[string]interface{} {
-
-	for _, value := range module.Variables {
-		//if value.Value == nil {
-		fmt.Printf("%s: %s\n", value.Name, value.Value)
-		//}
-	}
-
-	return nil
 }
